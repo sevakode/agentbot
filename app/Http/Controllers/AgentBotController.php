053cfe86c8bcec3ee14dc6f85c15ecf2951540e2
Seva <sevakode@gmail.com>
2024-04-11 05:12:57 +0700

    Add AgentBotController for handling agent-specific bots
    - Create new AgentBotController with handleAgentBot method
    - Load TelegramDriver and create BotMan instance with provided token
    - Define agent config with start and reply webhook URLs
    - Handle /start command by sending last 20 messages to start URL
    - Handle agent replies by sending reply text to reply URL
    - Add new route in api.php for handling agent-specific bots

---
+++ app/Http/Controllers/AgentBotController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\Drivers\Telegram\TelegramDriver;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class AgentBotController extends Controller
+{
+    public function handleAgentBot(Request $request, $token, $agent_id)
+    {
+        DriverManager::loadDriver(TelegramDriver::class);
+        $botman = BotManFactory::create(['telegram' => $token]);
+
+        // $agentConfig = config("agents.agent{$agent_id}");
+        $agentConfig = [
+            "urlStart"=>"https://hooks.zapier.com/hooks/catch/9924015/e315a7ba13844659803548c962baf52e/",
+            "urlReply"=>"https://hooks.zapier.com/hooks/catch/9924015/703fb766fab94b668071fe757cc6a791/"
+        ];
+
+        $botman->hears('/start', function ($botman) use ($agentConfig) {
+            $this->handleStartCommand($botman, $agentConfig);
+        });
+
+        $botman->hears('', function ($botman) use ($agentConfig) {
+            $message = $botman->getMessage();
+            if ($message->getPayload()['reply_to_message']['from']['id'] == $botman->getUser()->getId()) {
+                $this->handleReply($message->getText(), $agentConfig);
+            }
+        });
+
+        $botman->listen();
+    }
+
+    protected function handleStartCommand($botman, $agentConfig)
+    {
+        $messages = $botman->getMessages()->slice(-20);
+        $this->sendMessagesToUrl($messages, $agentConfig['urlStart']);
+    }
+
+    protected function handleReply($replyText, $agentConfig)
+    {
+        $this->sendReplyToUrl($replyText, $agentConfig['urlReply']);
+    }
+
+    protected function sendMessagesToUrl($messages, $url)
+    {
+        Http::post($url, ['messages' => $messages->toArray()]);
+    }
+
+    protected function sendReplyToUrl($replyText, $url)
+    {
+        Http::post($url, ['reply' => $replyText]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AgentBotController;
 use App\Http\Controllers\BotManController;
 use Illuminate\Support\Facades\Route;
 
 Route::match(['get', 'post'], '/botman', [BotManController::class, 'handleMainBot']);
 Route::match(['get', 'post'], '/botman/{token}/chats/{messenger_id}', [BotManController::class, 'handleAnswerBot'])->name('webhook.botman.answer');
 Route::match(['get', 'post'], '/botman/{token}', [BotManController::class, 'handleDynamicBot']);
+Route::match(['get', 'post'], '/botman/{token}/agent/{agent}', [AgentBotController::class, 'handleAgentBot']);
