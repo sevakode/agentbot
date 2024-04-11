0f18817394eb27d089f04942067e6b03b0918323
Seva <sevakode@gmail.com>
2024-04-11 07:00:18 +0700

    fix

--- app/Http/Controllers/AgentBotController.php
+++ app/Http/Controllers/AgentBotController.php
@@ -15,7 +15,6 @@ class AgentBotController extends Controller
     {
         DriverManager::loadDriver(TelegramDriver::class);
         $botman = BotManFactory::create(['telegram' => $token]);
-
         $agentConfig = [
             "url" => "https://hooks.zapier.com/hooks/catch/9924015/e315a7ba13844659803548c962baf52e/",
             "botname"=>"JiraTasker_bot"
@@ -23,6 +22,10 @@ class AgentBotController extends Controller
 
         $botman->hears('/start', function ($botman) use ($agentConfig) {
             $this->handleStartCommand($botman, $agentConfig);
+        });
+
+        $botman->hears('/done', function ($botman) use ($agentConfig) {
+            $this->handleDoneCommand($botman, $agentConfig);
         });
 
         $botman->hears('', function ($botman) use ($agentConfig) {
@@ -45,7 +48,15 @@ class AgentBotController extends Controller
 
         // Отправка диалога на URL
         $this->sendDialogToUrl($dialog, $agentConfig['url']);
+    }
 
+    protected function handleDoneCommand($botman, $agentConfig)
+    {
+        $message = $botman->getMessage();
+        $chatId = $message->getPayload()['chat']['id'];
+
+        // Очистка кеша после получения команды /done
+        Cache::forget("dialog_{$chatId}");
     }
 
     protected function handleMessage($botman, $agentConfig)
@@ -53,38 +64,36 @@ class AgentBotController extends Controller
         $message = $botman->getMessage();
         $chatId = $message->getPayload()['chat']['id'];
         $messageText = $message->getText();
-        
+
         // Получение информации об отправителе сообщения из полезной нагрузки
         $sender = $message->getPayload()['from'];
         $firstName = $sender['first_name'];
         $lastName = $sender['last_name'] ?? '';
         $username = $sender['username'] ?? '';
-        
+
         // Объединение имени, фамилии и юзернейма в одну строку
         $userInfo = $firstName . ' ' . $lastName;
         if ($username) {
             $userInfo .= ' (@' . $username . ')';
         }
 
-        // Сохранение сообщения в кеш, если это не команда /start
-        if ($messageText !== '/start') {
+        // Сохранение сообщения в кеш, если это не команда /start или /done
+        if ($messageText !== '/start' && $messageText !== '/done') {
             $dialog = Cache::get("dialog_{$chatId}", []);
             $dialog['messages'][] = ['role' => 'user', 'content' => $messageText, 'userInfo' => $userInfo];
             $dialog['messages'] = array_slice($dialog['messages'], -50); // Сохранение только последних 50 сообщений
+
             Cache::put("dialog_{$chatId}", $dialog);
         }
 
         // Проверка, является ли сообщение ответом пользователя (цитатой)
         $payload = $message->getPayload();
-        if (isset($payload['reply_to_message']) && $payload['reply_to_message']['from']['username'] ==$agentConfig['botname']) {
+        if (isset($payload['reply_to_message']) && $payload['reply_to_message']['from']['username'] == $agentConfig['botname']) {
             // Добавление идентификатора группы (чата) в диалог
             $dialog['chatId'] = $chatId;
 
             // Отправка диалога на URL
             $this->sendDialogToUrl($dialog, $agentConfig['url']);
-
-            // Очистка кеша после отправки ответа на реплику
-            Cache::forget("dialog_{$chatId}");
         }
     }
 
